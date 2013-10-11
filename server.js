d1d9cf13ea256610cd1973555b9b297631e4301f
Andrew Rhyne <rhyneandrew@gmail.com>
2013-10-11 16:19:02 -0700

    minor server-side tweaks

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express'),
-    server = express();
+    server = express(),
+    port =  (/\d/).test(process.argv[2]) ? process.argv[2] : 8080; //default to 8008, but use the first parameter if it is a number!
 
 server.use('/',express.static(__dirname+"/dist/"));
 
-server.listen(8080);
+console.log(process.argv, port);
+
+server.listen(port);
