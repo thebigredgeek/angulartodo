bc098a1207dfaa7777b5f487174715447bb7d2ea
Andrew Rhyne <rhyneandrew@gmail.com>
2013-10-11 16:31:30 -0700

    added travis-ci

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express'),
     server = express(),
+    cors = require('cors'),
     port =  (/\d/).test(process.argv[2]) ? process.argv[2] : 8080; //default to 8008, but use the first parameter if it is a number!
 
-server.use('/',express.static(__dirname+"/dist/"));
 
-console.log(process.argv, port);
+server.use(cors()); //automagic preflight
+
+server.use('/',express.static(__dirname+"/dist/")); //static route
 
 server.listen(port);
